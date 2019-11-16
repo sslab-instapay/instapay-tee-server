7e793f2b83239eeefc512acfd0721f999280b674
dongmin <xiaofo09@gmail.com>
2019-11-17 01:43:43 +0900

    자잘한 수정

--- grpc/grpc_server.go
+++ grpc/grpc_server.go
@@ -13,6 +13,7 @@ import (
 	"log"
 	"fmt"
 	"os"
+	"sync"
 	"context"
 	"strconv"
 	"google.golang.org/grpc"
@@ -21,114 +22,189 @@ import (
 	pbClient "github.com/sslab-instapay/instapay-go-server/proto/client"
 )
 
+
 type ServerGrpc struct {
 }
 
-/* wrapper function */
+
+var rwMutex = new(sync.RWMutex)
+
+
+func SendAgreementRequest(pn int64, address string, w pbClient.AgreeRequestsMessage) {
+	info, err := repository.GetClientInfo(address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clientAddr := (*info).IP + ":" + strconv.Itoa((*info).Port)
+	conn, err := grpc.Dial(clientAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	client := pbClient.NewClientClient(conn)
+	message := w
+	_, err = client.AgreementRequest(context.Background(), &message)
+	if err != nil {
+		log.Println(err)
+	}
+
+	rwMutex.Lock()
+	C.ecall_update_sentagr_list_w(C.uint(pn), &([]C.uchar(address))[0])
+	rwMutex.Unlock()
+
+	return
+}
+
+
+func SendUpdateRequest(pn int64, address string, w pbClient.AgreeRequestsMessage) {
+	info, err := repository.GetClientInfo(address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clientAddr := (*info).IP + ":" + strconv.Itoa((*info).Port)
+	conn, err := grpc.Dial(clientAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	client := pbClient.NewClientClient(conn)
+	rqm := pbClient.UpdateRequestsMessage{		/* convert AgreeRequestsMessage to UpdateRequestsMessage */
+		PaymentNumber:   w.PaymentNumber,
+		ChannelPayments: w.ChannelPayments,
+		Amount:          w.Amount}
+	_, err = client.UpdateRequest(context.Background(), &rqm)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rwMutex.Lock()
+	C.ecall_update_sentupt_list_w(C.uint(pn), &([]C.uchar(address))[0])
+	rwMutex.Unlock()
+	
+	return
+}
+
+
+func SendConfirmPayment(pn int, address string) {
+	info, err := repository.GetClientInfo(address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clientAddr := (*info).IP + ":" + strconv.Itoa((*info).Port)
+	conn, err := grpc.Dial(clientAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	client := pbClient.NewClientClient(conn)
+	_, err = client.ConfirmPayment(context.Background(), &pbClient.ConfirmRequestsMessage{PaymentNumber: int64(pn)})
+	if err != nil {
+		log.Fatal(err)
+	}	
+}
+
+
 func WrapperAgreementRequest(pn int64, p []string, w map[string]pbClient.AgreeRequestsMessage) {
 	/* remove C's address from p */
 	var q []string
 	q = p[0:2]
 
 	for _, address := range q {
-		info, err := repository.GetClientInfo(address)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		clientAddr := (*info).IP + ":" + strconv.Itoa((*info).Port)
-		conn, err := grpc.Dial(clientAddr, grpc.WithInsecure())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		defer conn.Close()
-
-		client := pbClient.NewClientClient(conn)
-		message := w[address]
-		_, err = client.AgreementRequest(context.Background(), &message)
-		if err != nil {
-			log.Println(err)
-		}
-
-		C.ecall_update_sentagr_list_w(C.uint(pn), &([]C.uchar(address))[0])
-
-		if C.ecall_check_unanimity_w(C.uint(pn), C.int(0)) == 1 {
-			go WrapperUpdateRequest(pn, p, w)
-			return
-		}
+		go SendAgreementRequest(pn, address, w[address])
 	}
+
+	for C.ecall_check_unanimity_w(C.uint(pn), C.int(0)) != 1 {}
+
+	go WrapperUpdateRequest(pn, p, w)
 }
+
 
 func WrapperUpdateRequest(pn int64, p []string, w map[string]pbClient.AgreeRequestsMessage) {
 	for _, address := range p {
-		info, err := repository.GetClientInfo(address)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		clientAddr := (*info).IP + ":" + strconv.Itoa((*info).Port)
-		conn, err := grpc.Dial(clientAddr, grpc.WithInsecure())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		defer conn.Close()
-
-		client := pbClient.NewClientClient(conn)
-		rqm := pbClient.UpdateRequestsMessage{		/* convert AgreeRequestsMessage to UpdateRequestsMessage */
-			PaymentNumber:   w[address].PaymentNumber,
-			ChannelPayments: w[address].ChannelPayments,
-			Amount:          w[address].Amount}
-		_, err = client.UpdateRequest(context.Background(), &rqm)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		C.ecall_update_sentupt_list_w(C.uint(pn), &([]C.uchar(address))[0])
-
-		if C.ecall_check_unanimity_w(C.uint(pn), C.int(1)) == 1 {
-			go WrapperConfirmPayment(int(pn), p)
-			return
-		}
+		go SendUpdateRequest(pn, address, w[address])
 	}
+
+	for C.ecall_check_unanimity_w(C.uint(pn), C.int(1)) != 1 {}
+
+	go WrapperConfirmPayment(int(pn), p)
 }
+
 
 func WrapperConfirmPayment(pn int, p []string) {
 	/* update payment's status */
 	C.ecall_update_payment_status_to_success_w(C.uint(pn))
 
 	for _, address := range p {
-		info, err := repository.GetClientInfo(address)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		clientAddr := (*info).IP + ":" + strconv.Itoa((*info).Port)
-		conn, err := grpc.Dial(clientAddr, grpc.WithInsecure())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		defer conn.Close()
-
-		client := pbClient.NewClientClient(conn)
-		_, err = client.ConfirmPayment(context.Background(), &pbClient.ConfirmRequestsMessage{PaymentNumber: int64(pn)})
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("======== Sent confirm to: " + clientAddr)
+		go SendConfirmPayment(pn, address)
+		fmt.Println("SENT CONFIRMATION")
 	}
 }
 
+
 func SearchPath(pn int64, amount int64) ([]string, map[string]pbClient.AgreeRequestsMessage) {
+	var p []string
+
+	/* composing p */
+	p = []string{
+		"D03A2CC08755eC7D75887f0997195654b928893e",
+		"0b4161ad4f49781a821C308D672E6c669139843C",
+		"78902c58006916201F65f52f7834e467877f0500"}
+
+	/* composing w */
+	amount = int64(amount)
+	pn = int64(pn)
+
+	var w map[string]pbClient.AgreeRequestsMessage
+	w = make(map[string]pbClient.AgreeRequestsMessage)
+
+	var cps1 []*pbClient.ChannelPayment
+	cps1 = append(cps1, &pbClient.ChannelPayment{ChannelId: int64(1), Amount: -amount})
+	rqm1 := pbClient.AgreeRequestsMessage{
+		PaymentNumber:   pn,
+		ChannelPayments: &pbClient.ChannelPayments{ChannelPayments: cps1},
+		Amount:          amount}
+	w["D03A2CC08755eC7D75887f0997195654b928893e"] = rqm1
+
+	var cps2 []*pbClient.ChannelPayment
+	cps2 = append(cps2, &pbClient.ChannelPayment{ChannelId: int64(1), Amount: amount})
+	cps2 = append(cps2, &pbClient.ChannelPayment{ChannelId: int64(2), Amount: -amount})
+	rqm2 := pbClient.AgreeRequestsMessage{
+		PaymentNumber:   pn,
+		ChannelPayments: &pbClient.ChannelPayments{ChannelPayments: cps2},
+		Amount:          amount}
+	w["0b4161ad4f49781a821C308D672E6c669139843C"] = rqm2
+
+	var cps3 []*pbClient.ChannelPayment
+	cps3 = append(cps3, &pbClient.ChannelPayment{ChannelId: int64(2), Amount: amount})
+	rqm3 := pbClient.AgreeRequestsMessage{
+		PaymentNumber:   pn,
+		ChannelPayments: &pbClient.ChannelPayments{ChannelPayments: cps3},
+		Amount:          amount}
+	w["78902c58006916201F65f52f7834e467877f0500"] = rqm3
+
+	return p, w
+}
+
+
+func SearchPath2222(pn int64, amount int64) ([]string, map[string]pbClient.AgreeRequestsMessage) {
 	var p []string
 	var channelID1 int64
 	var channelID2 int64
 
 	/* composing p */
-	p = []string{"0xD03A2CC08755eC7D75887f0997195654b928893e", "0x0b4161ad4f49781a821C308D672E6c669139843C", "0x78902c58006916201F65f52f7834e467877f0500"}
+	p = []string{
+		"D03A2CC08755eC7D75887f0997195654b928893e",
+		"0b4161ad4f49781a821C308D672E6c669139843C",
+		"78902c58006916201F65f52f7834e467877f0500"}
 
 	/* composing w */
 	channels, err := repository.GetChannelList()
@@ -137,9 +213,9 @@ func SearchPath(pn int64, amount int64) ([]string, map[string]pbClient.AgreeRequ
 	}
 
 	for i := 0; i < len(channels); i++ {
-		if channels[i].From == "0xD03A2CC08755eC7D75887f0997195654b928893e" {
+		if channels[i].From == "D03A2CC08755eC7D75887f0997195654b928893e" {
 			channelID1 = int64(channels[i].ChannelId)
-		} else if channels[i].From == "0x0b4161ad4f49781a821C308D672E6c669139843C" {
+		} else if channels[i].From == "0b4161ad4f49781a821C308D672E6c669139843C" {
 			channelID2 = int64(channels[i].ChannelId)
 		}
 	}
@@ -158,7 +234,7 @@ func SearchPath(pn int64, amount int64) ([]string, map[string]pbClient.AgreeRequ
 		PaymentNumber:   pn,
 		ChannelPayments: &pbClient.ChannelPayments{ChannelPayments: cps1},
 		Amount:          amount}
-	w["0xD03A2CC08755eC7D75887f0997195654b928893e"] = rqm1
+	w["D03A2CC08755eC7D75887f0997195654b928893e"] = rqm1
 
 	var cps2 []*pbClient.ChannelPayment
 	cps2 = append(cps2, &pbClient.ChannelPayment{ChannelId: channelID1, Amount: amount})
@@ -167,7 +243,7 @@ func SearchPath(pn int64, amount int64) ([]string, map[string]pbClient.AgreeRequ
 		PaymentNumber:   pn,
 		ChannelPayments: &pbClient.ChannelPayments{ChannelPayments: cps2},
 		Amount:          amount}
-	w["0x0b4161ad4f49781a821C308D672E6c669139843C"] = rqm2
+	w["0b4161ad4f49781a821C308D672E6c669139843C"] = rqm2
 
 	var cps3 []*pbClient.ChannelPayment
 	cps3 = append(cps3, &pbClient.ChannelPayment{ChannelId: channelID2, Amount: amount})
@@ -175,10 +251,11 @@ func SearchPath(pn int64, amount int64) ([]string, map[string]pbClient.AgreeRequ
 		PaymentNumber:   pn,
 		ChannelPayments: &pbClient.ChannelPayments{ChannelPayments: cps3},
 		Amount:          amount}
-	w["0x78902c58006916201F65f52f7834e467877f0500"] = rqm3
+	w["78902c58006916201F65f52f7834e467877f0500"] = rqm3
 
 	return p, w
 }
+
 
 func (s *ServerGrpc) PaymentRequest(ctx context.Context, rq *pbServer.PaymentRequestMessage) (*pbServer.Result, error) {
 	from := rq.From
@@ -194,11 +271,13 @@ func (s *ServerGrpc) PaymentRequest(ctx context.Context, rq *pbServer.PaymentReq
 	for i := 0; i < len(p); i++ {
 		C.ecall_add_participant_w(PaymentNum, &([]C.uchar(p[i]))[0])
 	}
+	C.ecall_update_sentagr_list_w(PaymentNum, &([]C.uchar(p[2]))[0])
 
 	go WrapperAgreementRequest(int64(PaymentNum), p, w)
 
 	return &pbServer.Result{Result: true}, nil
 }
+
 
 func (s *ServerGrpc) CommunicationInfoRequest(ctx context.Context, address *pbServer.Address) (*pbServer.CommunicationInfo, error) {
 	res, err := repository.GetClientInfo(address.Addr)
@@ -208,6 +287,7 @@ func (s *ServerGrpc) CommunicationInfoRequest(ctx context.Context, address *pbSe
 
 	return &pbServer.CommunicationInfo{IPAddress: res.IP, Port: int64(res.Port)}, nil
 }
+
 
 func StartGrpcServer() {
 	grpcPort, err := strconv.Atoi(os.Getenv("grpc_port"))
